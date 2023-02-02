95c46cdb176afb40f97611b6616419f6f2536cbc
Ainaa Azman <ainaasakinah@gmail.com>
2023-02-02 18:52:20 +0900

    add today\'s thoughts and unit test progress

--- chapter-5/expense_tracker/app/api.rb
+++ chapter-5/expense_tracker/app/api.rb
@@ -9,7 +9,14 @@ module ExpenseTracker
     end
 
     post '/expenses' do
-      JSON.generate('expense_id' => 42)
+      # parse an expense from the request body
+      expense = JSON.parse(request.body.read)
+
+      # Use it's ledger to record the expense
+      result = @ledger.record(expense)
+
+      # return a JSON document containing the resulting expense ID
+      JSON.generate('expense_id' => result.expense_id)
     end
 
     get '/expenses/:date' do
